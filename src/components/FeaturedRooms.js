3246832f75a954f42a1bc8e61dcd2b40b1917608
Malaika <malaikafernandes25@gmail.com>
2020-10-08 16:14:41 +0530

    room detail page added

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
-import {RoomContext} from '../context'
+import {RoomContext} from '../context';
+import Loading from './Loading'
+import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
-    static contextType = RoomContext
+    static contextType = RoomContext;
+   
     render() {
-        const {greeting, name}= this.context;
-        // 1:50;
+        let {loading, featuredRooms: rooms} = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+    
         return (
-            <div>
-                Hello from featured rooms {greeting}
-            </div>
+            <section className="featured-rooms">
+                <Title title="Featured rooms" />
+                <div className="featured-rooms-center">
+                    {loading? <Loading /> : rooms }
+                </div>
+            </section>
         )
     }
 }
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,75 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../context'
+import StyledHero from '../components/StyledHero'
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+    static contextType = RoomContext;
     render() {
+        const {getRoom} = this.context;
+        const room= getRoom(this.state.slug);
+       if(!room){
+           return <div className="error">
+               <h3> No Such room could be found</h3>
+               <Link to="/rooms" className="btn-primary">
+                   Back to rooms
+               </Link>
+           </div>
+       }
+       const {name, description, capacity, size, price, extras, breakfast,pets,images} = room;
+       const [mainImg, ...defaultImg] = images;
         return (
-            <div>
-                Hello from single room page
-            </div>
+            <>
+                <StyledHero  img={mainImg || this.state.defaultBcg}>
+                    <Banner title={`${name} room`}>
+                        <Link to="/rooms" className="btn-primary">
+                            Back to rooms
+                        </Link>
+                    </Banner>
+                </StyledHero>
+                <section className="single-room">
+                    <div classname="single-room-iimages">
+                        {defaultImg.map((item, index)=>{
+                            return <img key={index} src={item} alt={name} />
+                        })}
+                    </div>
+                    <div classname="single-room-info">
+                        <article class="desc">
+                            <h3>Details</h3>
+                            <p>{description}</p>
+
+                        </article>
+                        <article class="info">
+                            <h3>Info</h3>
+                            <h6>Price: ${price}</h6>
+                            <h6> Size: {size} sq ft</h6>
+                            <h6> 
+                                Max Capacity : {capacity} {capacity > 1 ? 'people' : 'person'}
+                            </h6>
+                            <h6> {pets ? 'Pets Allowed' : 'No pets Allowed'}</h6>
+                            <h6>{breakfast && "Free Breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+                <section className="room-extras">
+                    <h6>Extras</h6>
+                    <ul className="extras">
+                        {extras.map((item, index)=>{
+                           return <li key={index}>- {item}</li>
+                        })}
+                    </ul>
+                </section>
+           </>
         )
     }
 }
