1636bcfe42eeec000190f188ad62638fff5bb7ea
Malaika <malaikafernandes25@gmail.com>
2020-10-12 18:50:04 +0530

    room filters completed

--- src/context.js
+++ src/context.js
@@ -58,7 +58,7 @@ class RoomProvider extends Component {
     handleChange = event =>{
         const target= event.target;
         const name= event.target.name;
-        const value= event.type ==='checkbox'?event.target.checked: event.target.value;
+        const value= target.type ==='checkbox'?event.target.checked: event.target.value;
         this.setState({
             [name]: value
         }, this.filterRooms)
@@ -75,10 +75,34 @@ class RoomProvider extends Component {
             breakfast,
             pets 
         } = this.state;
-
+        //all the rooms
         let tempRooms = [...rooms];
+        //transform values
+        capacity = parseInt(capacity);
+        price = parseInt(price);
+        //filter by type
         if(type!=='all'){
             tempRooms = tempRooms.filter(room => room.type === type)
+        }
+
+        //filter by capacity
+        if(capacity!==1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        //filter by price
+        tempRooms= tempRooms.filter(room => room.price <= price)
+
+        //filter by size
+        tempRooms = tempRooms.filter(room => room.size>=minSize && room.size <=maxSize)
+
+        // filter by breakfast
+        if(breakfast){
+            tempRooms = tempRooms.filter(room=> room.breakfast===true)
+        }
+        // filter by pets
+        if(pets){
+            tempRooms = tempRooms.filter(room=> room.pets===true)
         }
         this.setState({
             sortedRooms: tempRooms
